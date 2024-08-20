54bc53df42b976260311ff1c7c9023a3add42733
vudovenko <vladislav.udovenko0@yandex.ru>
2024-08-20 20:26:27 +0500

    создал микросервис для работы со вспомогательными сущностями

---
+++ planner-todo/src/main/java/ru/vudovenko/micro/planner/todo/PlannerTodoApplication.java
@@ -0,0 +1,15 @@
+package ru.vudovenko.micro.planner.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PlannerTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerTodoApplication.class, args);
+	}
+
+}
---
+++ planner-todo/src/test/java/ru/vudovenko/micro/planner/todo/PlannerTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vudovenko.micro.planner.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
