4e46aa4b7178a60218d9157295b74b4eef97b96a
vudovenko <vladislav.udovenko0@yandex.ru>
2024-08-23 18:08:11 +0500

    1) создан модуль utils для aop 2) подключен в planner-todo

---
+++ planner-utils/src/main/java/ru/vudovenko/micro/planner/plannerutils/aop/LoggingAspect.java
@@ -0,0 +1,37 @@
+package ru.vudovenko.micro.planner.plannerutils.aop;
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+@Log4j2
+public class LoggingAspect {
+
+    @Around("execution(* ru.vudovenko.micro.planner.todo.controllers..*(..)))")
+    public Object profileControllerMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        log.debug("-------- Executing " + className + "." + methodName + "   ----------- ");
+
+        final StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Object result = proceedingJoinPoint.proceed();
+        stopWatch.stop();
+
+        log.debug("-------- Execution time of " + className
+                + "." + methodName
+                + ":: " + stopWatch.getTotalTimeMillis() + " ms"
+                + " ----------- ");
+
+        return result;
+    }
+}
