44f9eb46ad359a806a12744b144b295a3ccb7ea9
vudovenko <vladislav.udovenko0@yandex.ru>
2024-08-25 13:11:51 +0500

    создан модуль planner-users

---
+++ planner-users/src/main/java/ru/vudovenko/micro/planner/users/PlannerUsersApplication.java
@@ -0,0 +1,13 @@
+package ru.vudovenko.micro.planner.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerUsersApplication.class, args);
+	}
+
+}
