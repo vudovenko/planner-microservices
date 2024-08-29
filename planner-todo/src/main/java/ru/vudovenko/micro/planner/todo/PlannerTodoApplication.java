c926a87827e2ca6bba5859aeba13b148eda690a8
vudovenko <vladislav.udovenko0@yandex.ru>
2024-08-29 23:40:38 +0500

    динамическое считывание измененных настроек с помощью запроса к actuator

--- planner-todo/src/main/java/ru/vudovenko/micro/planner/todo/PlannerTodoApplication.java
+++ planner-todo/src/main/java/ru/vudovenko/micro/planner/todo/PlannerTodoApplication.java
@@ -3,10 +3,12 @@ package ru.vudovenko.micro.planner.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@RefreshScope
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
--- planner-users/src/main/java/ru/vudovenko/micro/planner/users/PlannerUsersApplication.java
+++ planner-users/src/main/java/ru/vudovenko/micro/planner/users/PlannerUsersApplication.java
@@ -3,9 +3,11 @@ package ru.vudovenko.micro.planner.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@RefreshScope
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(basePackages = {"ru.vudovenko.micro.planner"})
